b7bfc8b0b6183be925ba2a79747ee54537c96f6d
Antoine Gibek <antoine.gibek@ipsl.fr>
2023-08-07 16:02:23 +0200

    feat implements path parser

--- src/path_parser.rs
+++ src/path_parser.rs
@@ -1,32 +1,32 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Add};
 
 fn is_word_char(c: char) -> bool {
     c.is_alphanumeric() || c == '_'
 }
+#[derive(Debug, PartialEq)]
 struct Placeholder {
     full_placeholder: String,
     placeholders: Vec<String>,
 }
-
 fn parse_percent_placeholder(iter: &mut std::iter::Peekable<std::str::Chars>) -> Placeholder {
-    let mut full_placeholder = "%".to_string();
-    let mut current_placeholder = "%".to_string();
+    let mut full_placeholder = String::from("%");
+    let mut current_placeholder = String::from("%");
     let mut placeholders = Vec::new();
-    println!("iter: {:?}", iter);
+
     iter.next();
-    while let Some(next_char) = iter.next() {
+    while let Some(&next_char) = iter.peek() {
         if is_word_char(next_char) {
             current_placeholder.push(next_char);
             full_placeholder.push(next_char);
-            continue;
-        }
-
-        if next_char == '%' {
+            iter.next();
+        } else if next_char == '%' {
             placeholders.push(current_placeholder.clone());
             current_placeholder.clear();
+            iter.next();
+            break;
+        } else {
             break;
         }
-        break;
     }
     placeholders.push(current_placeholder.clone());
     Placeholder {
@@ -36,30 +36,40 @@ fn parse_percent_placeholder(iter: &mut std::iter::Peekable<std::str::Chars>) ->
 }
 
 fn parse_curly_placeholder(iter: &mut std::iter::Peekable<std::str::Chars>) -> Placeholder {
-    let mut full_placeholder = "{".to_string();
-    let mut current_placeholder = "{".to_string();
+    let mut full_placeholder = String::from("{");
+    let mut current_placeholder = String::from("{");
     let mut placeholders = Vec::new();
+    let mut is_in_fallback = false;
 
     iter.next();
-    while let Some(next_char) = iter.next() {
+    while let Some(&next_char) = iter.peek() {
         if next_char == '}' {
-            full_placeholder.push('}');
+            if is_in_fallback {
+                placeholders.push(current_placeholder.clone());
+            }
+            full_placeholder.push(next_char);
+            iter.next();
             break;
         }
-        if is_word_char(next_char) {
-            current_placeholder.push(next_char);
-            full_placeholder.push(next_char);
+        if next_char == '%' {
+            is_in_fallback = false;
+            let placeholder = parse_percent_placeholder(iter);
+            full_placeholder.push_str(&placeholder.full_placeholder);
+            placeholders.extend(placeholder.placeholders);
+            current_placeholder.clear();
             continue;
         }
-        if next_char == '%' {
-            let placeholder = parse_percent_placeholder(iter);
-            placeholders.extend(placeholder.placeholders);
-        } else if next_char == '|' || next_char == ' ' {
-            placeholders.push(current_placeholder.clone());
+        if next_char == '|' {
+            is_in_fallback = true;
+            current_placeholder.clear();
+            full_placeholder.push(next_char);
+            iter.next();
+            continue;
+        } else {
+            current_placeholder.push(next_char);
+            full_placeholder.push(next_char);
+            iter.next();
         }
-
-        current_placeholder.clear();
-        full_placeholder.push(next_char);
     }
 
     Placeholder {
@@ -81,6 +91,7 @@ pub fn get_placeholders_map(raw_path: &str) -> HashMap<String, Vec<String>> {
                     .or_insert(vec![])
                     .extend(placeholder.placeholders);
             }
+
             '{' => {
                 let placeholder = parse_curly_placeholder(&mut iter);
                 placeholder_map
@@ -88,6 +99,7 @@ pub fn get_placeholders_map(raw_path: &str) -> HashMap<String, Vec<String>> {
                     .or_insert(vec![])
                     .extend(placeholder.placeholders);
             }
+
             _ => {
                 iter.next(); // Skip the character if it is not '%' or '{'
             }
@@ -122,7 +134,7 @@ mod tests {
         let result = parse_curly_placeholder(&mut iter);
 
         let expected = Placeholder {
-            full_placeholder: "%placeholder".to_string(),
+            full_placeholder: "{%placeholder}".to_string(),
             placeholders: vec!["%placeholder".to_string()],
         };
 
@@ -137,7 +149,7 @@ mod tests {
         let result = parse_curly_placeholder(&mut iter);
 
         let expected = Placeholder {
-            full_placeholder: "%placeholder".to_string(),
+            full_placeholder: "{%placeholder|%second_placeholder}".to_string(),
             placeholders: vec![
                 "%placeholder".to_string(),
                 "%second_placeholder".to_string(),
@@ -166,28 +178,22 @@ mod tests {
         assert_eq!(result.full_placeholder, expected.full_placeholder);
     }
     #[test]
-    fn test_parse_get_placeholders_map() {
-        let input = "/home/myuser/photos/%year/{%city|%event|To sort}";
-        let result = get_placeholders_map(input);
+    fn test_parse_curly_placeholder_multiple_last_fallback_empty() {
+        let input = "{%placeholder|%second_placeholder|}";
+        let mut iter = input.chars().peekable();
+        let result = parse_curly_placeholder(&mut iter);
 
-        let mut expected = HashMap::new();
-        expected.insert("%placeholder".to_string(), vec!["%placeholder".to_string()]);
+        let expected = Placeholder {
+            full_placeholder: "{%placeholder|%second_placeholder|}".to_string(),
+            placeholders: vec![
+                "%placeholder".to_string(),
+                "%second_placeholder".to_string(),
+                "".to_string(),
+            ],
+        };
 
-        assert_eq!(result, expected);
-    }
-    #[test]
-    fn test_parse_get_placeholders_map_multiple_in_touch() {
-        let input = "/home/myuser/photos/%year/{%city|%event|To sort}";
-        let result = get_placeholders_map(input);
-
-        let mut expected = HashMap::new();
-        expected.insert("%placeholder".to_string(), vec!["%placeholder".to_string()]);
-        expected.insert(
-            "{%placeholder}".to_string(),
-            vec!["%placeholder".to_string()],
-        );
-
-        assert_eq!(result, expected);
+        assert_eq!(result.placeholders, expected.placeholders);
+        assert_eq!(result.full_placeholder, expected.full_placeholder);
     }
     #[test]
     fn test_parse_get_placeholders_map_full_path() {
