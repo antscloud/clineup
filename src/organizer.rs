1b8b8464eefb61db7b8f65901215bf529299cd9e
Antoine Gibek <antoine.gibek@gmail.com>
2023-09-06 16:46:41 +0200

    feat: add organization methods

--- src/organizer.rs
+++ src/organizer.rs
@@ -1,14 +1,106 @@
-#[derive(Debug, Deserialize, Serialize)]
-enum OrganizationMode {
+use log::{error, info};
+use serde::{Deserialize, Serialize};
+use std::os::unix::fs::symlink as unix_symlink;
+use std::path::PathBuf;
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub enum OrganizationMode {
     Symlinks,
-    InPlace,
+    Move,
     Copy,
 }
 
-trait Strategy {
-    pub fn organize() {}
+pub trait OrganizationStrategy {
+    fn organize(&self, original_file: &PathBuf, destination: &PathBuf) {}
 }
 
-struct CopyStrategy {}
-struct SymlinksStrategy {}
-struct InplaceStrategy {}
+pub struct CopyStrategy {}
+impl CopyStrategy {
+    pub fn new() -> CopyStrategy {
+        CopyStrategy {}
+    }
+}
+impl OrganizationStrategy for CopyStrategy {
+    fn organize(&self, original_file: &PathBuf, destination: &PathBuf) {
+        info!(
+            "Copying {} to {}",
+            original_file.display(),
+            destination.display()
+        );
+
+        if let Some(parent) = destination.parent() {
+            std::fs::create_dir_all(parent);
+        };
+
+        if destination.exists() {
+            error!("Destination file already exists. Aborting copy.");
+            return;
+        }
+
+        let copy_result = std::fs::copy(original_file, destination);
+
+        match copy_result {
+            Ok(_) => info!("File copied successfully"),
+            Err(e) => error!("Error copying file: {}", e),
+        }
+    }
+}
+pub struct SymlinksStrategy {}
+impl SymlinksStrategy {
+    pub fn new() -> SymlinksStrategy {
+        SymlinksStrategy {}
+    }
+}
+
+impl OrganizationStrategy for SymlinksStrategy {
+    fn organize(&self, original_file: &PathBuf, destination: &PathBuf) {
+        info!(
+            "Symlinking {} to {}",
+            destination.display(),
+            original_file.display(),
+        );
+        if let Some(parent) = destination.parent() {
+            std::fs::create_dir_all(parent);
+        };
+
+        if destination.exists() {
+            error!("Destination file already exists. Aborting copy.");
+            return;
+        }
+
+        let symlink_result = unix_symlink(original_file, destination);
+        match symlink_result {
+            Ok(_) => info!("File symlinked successfully"),
+            Err(e) => error!("Error symlinking file: {}", e),
+        }
+    }
+}
+pub struct MoveStrategy {}
+
+impl MoveStrategy {
+    pub fn new() -> MoveStrategy {
+        MoveStrategy {}
+    }
+}
+impl OrganizationStrategy for MoveStrategy {
+    fn organize(&self, original_file: &PathBuf, destination: &PathBuf) {
+        info!(
+            "Moving {} to {}",
+            original_file.display(),
+            destination.display(),
+        );
+        if let Some(parent) = destination.parent() {
+            std::fs::create_dir_all(parent);
+        };
+
+        if destination.exists() {
+            error!("Destination file already exists. Aborting copy.");
+            return;
+        }
+        let move_result = std::fs::rename(original_file, destination);
+        match move_result {
+            Ok(_) => info!("File moved successfully"),
+            Err(e) => error!("Error moving file: {}", e),
+        }
+    }
+}
