2e382152f83b82e256bdfa42238f90c84122e70c
Antoine Gibek <antoine.gibek@gmail.com>
2023-09-18 13:06:25 +0200

    refactor: Move utils functions from main

--- src/main.rs
+++ src/main.rs
@@ -2,109 +2,20 @@ use clineup::cli::check_cli_config_from_placeholders;
 use clineup::cli::get_cli_config;
 use clineup::cli::parse_cli;
 use clineup::cli::set_log_level;
-use clineup::cli::Config;
-use clineup::gps::base::GpsResolutionProvider;
-use clineup::gps::gpsenum::GpsResolutionProviderImpl;
-use clineup::gps::nominatim::Nominatim;
-use clineup::organizer::CopyStrategy;
-use clineup::organizer::MoveStrategy;
-use clineup::organizer::OrganizationMode;
-use clineup::organizer::OrganizationStrategy;
-use clineup::organizer::SymlinksStrategy;
 use clineup::path::duplicates_finder::DuplicatesFinder;
 use clineup::path::formatter::PathFormatter;
 use clineup::path::iterator::FileIterator;
 use clineup::path::parser::map_placeholders_to_enums;
 use clineup::path::parser::parse_placeholders;
 
+use clineup::utils::get_full_format_path;
+use clineup::utils::get_organization_strategy;
+use clineup::utils::get_reverse_geocoding;
 use log::debug;
 use log::error;
 use log::info;
 use std::path::Path;
-use std::path::PathBuf;
 use std::process::exit;
-
-/// Constructs a full format path based on the provided folder and filename formats.
-///
-/// # Arguments
-///
-/// * `folder_format` - An optional string representing the folder format.
-/// * `filename_format` - An optional string representing the filename format.
-///
-/// # Returns
-///
-/// An optional `String` representing the full format path, or `None` if both `folder_format` and `filename_format` are `None`.
-fn get_full_format_path(
-    folder_format: Option<&String>,
-    filename_format: Option<&String>,
-) -> Option<String> {
-    let mut full_format: Option<PathBuf> = None;
-
-    if let Some(folder_format) = folder_format {
-        full_format = Some(Path::new(&folder_format).to_path_buf());
-    }
-
-    if let Some(filename_format) = filename_format {
-        let _format = Path::new(&filename_format).to_path_buf();
-        if let Some(mut outer_full_format) = full_format.take() {
-            outer_full_format.push(_format);
-            full_format = Some(outer_full_format);
-        } else {
-            full_format = Some(_format);
-        }
-    }
-
-    // Convert the final PathBuf into an Option<String>
-    full_format.map(|path| path.to_string_lossy().to_string())
-}
-
-/// Retrieves the reverse geocoding provider based on the given configuration.
-///
-/// # Arguments
-///
-/// * `config` - The configuration object.
-///
-/// # Returns
-///
-/// An `Option<Box<dyn GpsResolutionProvider>>` representing the reverse geocoding provider.
-pub fn get_reverse_geocoding(config: &Config) -> Option<Box<dyn GpsResolutionProvider>> {
-    match &config.reverse_geocoding {
-        Some(provider) => match provider {
-            GpsResolutionProviderImpl::Nominatim => {
-                // Check if Nominatim email is provided
-                if config.nominatim_email.is_none() {
-                    error!("Nominatim email is required when using Nominatim as a reverse geocoding provider.");
-                    exit(1)
-                }
-                // Create a new Nominatim instance with the provided email
-                Some(Box::new(Nominatim::new(
-                    config.nominatim_email.clone().unwrap(),
-                )))
-            }
-        },
-        None => None,
-    }
-}
-
-/// Returns an organization strategy based on the given config.
-///
-/// # Arguments
-///
-/// * `config` - The configuration object.
-///
-/// # Returns
-///
-/// An `Option` containing a boxed organization strategy, or `None` if no strategy is specified in the config.
-pub fn get_organization_strategy(config: &Config) -> Option<Box<dyn OrganizationStrategy>> {
-    match &config.strategy {
-        Some(strategy) => match strategy {
-            OrganizationMode::Copy => Some(Box::new(CopyStrategy::new())),
-            OrganizationMode::Symlinks => Some(Box::new(SymlinksStrategy::new())),
-            OrganizationMode::Move => Some(Box::new(MoveStrategy::new())),
-        },
-        None => None,
-    }
-}
 
 fn main() {
     let matches = parse_cli();
@@ -113,7 +24,7 @@ fn main() {
 
     let config = get_cli_config(matches);
 
-    let strategy = get_organization_strategy(&config);
+    let strategy = get_organization_strategy(config.strategy.as_ref());
 
     if strategy.is_none() {
         error!("Strategy is not set");
--- src/utils.rs
+++ src/utils.rs
@@ -2,12 +2,27 @@ use std::{collections::HashMap, process::exit};
 
 use log::error;
 
+use crate::cli::Config;
+use crate::gps::base::GpsResolutionProvider;
+use crate::gps::gpsenum::GpsResolutionProviderImpl;
+use crate::gps::nominatim::Nominatim;
+use crate::organizer::CopyStrategy;
+use crate::organizer::MoveStrategy;
+use crate::organizer::OrganizationMode;
+use crate::organizer::OrganizationStrategy;
+use crate::organizer::SymlinksStrategy;
 use crate::{errors::ClineupError, placeholders::Placeholder};
 
+use std::path::Path;
+use std::path::PathBuf;
+
+/// Checks if there is an EXIF placeholder in the given placeholders.
+/// Returns `true` if there is at least one EXIF placeholder, `false` otherwise.
 pub fn is_there_a_exif_placeholder(
     placeholders: &HashMap<String, HashMap<String, Placeholder>>,
 ) -> bool {
     let mut is_there_a_date_placeholder = false;
+
     for (_, placeholders) in placeholders {
         for (_, placeholder) in placeholders {
             if placeholder.is_exif_related() {
@@ -16,9 +31,12 @@ pub fn is_there_a_exif_placeholder(
             }
         }
     }
+
     is_there_a_date_placeholder
 }
 
+/// Checks if there is an metadata placeholder in the given placeholders.
+/// Returns `true` if there is at least one metadata placeholder, `false` otherwise.
 pub fn is_there_a_metadata_placeholder(
     placeholders: &HashMap<String, HashMap<String, Placeholder>>,
 ) -> bool {
@@ -33,7 +51,8 @@ pub fn is_there_a_metadata_placeholder(
     }
     is_there_a_metadata_placeholder
 }
-
+/// Checks if there is an location related placeholder in the given placeholders.
+/// Returns `true` if there is at least one location related placeholder, `false` otherwise.
 pub fn is_there_a_location_placeholder(
     placeholders: &HashMap<String, HashMap<String, Placeholder>>,
 ) -> bool {
@@ -52,4 +71,152 @@ pub fn is_there_a_location_placeholder(
 pub fn print_error<T>(e: ClineupError) -> T {
     error!("{e}");
     exit(1)
+}
+
+/// Constructs a full format path based on the provided folder and filename formats.
+///
+/// # Arguments
+///
+/// * `folder_format` - An optional string representing the folder format.
+/// * `filename_format` - An optional string representing the filename format.
+///
+/// # Returns
+///
+/// An optional `String` representing the full format path, or `None` if both `folder_format` and `filename_format` are `None`.
+pub fn get_full_format_path(
+    folder_format: Option<&String>,
+    filename_format: Option<&String>,
+) -> Option<String> {
+    let mut full_format: Option<PathBuf> = None;
+
+    if let Some(_folder_format) = folder_format {
+        let mut _full_format = Path::new(&_folder_format).to_path_buf();
+        if let Some(_filename_format) = filename_format {
+            _full_format = _full_format.join(_filename_format);
+            full_format = Some(_full_format);
+        } else {
+            _full_format = _full_format.join("{%original_filename}");
+            full_format = Some(_full_format);
+        }
+    } else if let Some(_filename_format) = filename_format {
+        full_format = Some(Path::new(&_filename_format).to_path_buf());
+    }
+
+    // Convert the final PathBuf into an Option<String>
+    full_format.map(|path| path.to_string_lossy().to_string())
+}
+
+/// Retrieves the reverse geocoding provider based on the given configuration.
+///
+/// # Arguments
+///
+/// * `config` - The configuration object.
+///
+/// # Returns
+///
+/// An `Option<Box<dyn GpsResolutionProvider>>` representing the reverse geocoding provider.
+pub fn get_reverse_geocoding(config: &Config) -> Option<Box<dyn GpsResolutionProvider>> {
+    match &config.reverse_geocoding {
+        Some(provider) => match provider {
+            GpsResolutionProviderImpl::Nominatim => {
+                // Check if Nominatim email is provided
+                if config.nominatim_email.is_none() {
+                    error!("Nominatim email is required when using Nominatim as a reverse geocoding provider.");
+                    exit(1)
+                }
+                // Create a new Nominatim instance with the provided email
+                Some(Box::new(Nominatim::new(
+                    config.nominatim_email.clone().unwrap(),
+                )))
+            }
+        },
+        None => None,
+    }
+}
+
+pub fn get_organization_strategy(
+    strategy: Option<&OrganizationMode>,
+) -> Option<Box<dyn OrganizationStrategy>> {
+    match strategy {
+        Some(org_mode) => match org_mode {
+            OrganizationMode::Copy => Some(Box::new(CopyStrategy::new())),
+            OrganizationMode::Symlinks => Some(Box::new(SymlinksStrategy::new())),
+            OrganizationMode::Move => Some(Box::new(MoveStrategy::new())),
+        },
+        None => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_full_format_path() {
+        // Test case 1: Both folder_format and filename_format are None
+        assert_eq!(get_full_format_path(None, None), None);
+
+        // Test case 2: Only folder_format is provided
+        assert_eq!(
+            get_full_format_path(Some(&String::from("path/to/folder")), None),
+            Some(String::from("path/to/folder/{%original_filename}"))
+        );
+
+        // Test case 3: Only filename_format is provided
+        assert_eq!(
+            get_full_format_path(None, Some(&String::from("file.txt"))),
+            Some(String::from("file.txt"))
+        );
+
+        // Test case 4: Both folder_format and filename_format are provided
+        assert_eq!(
+            get_full_format_path(
+                Some(&String::from("path/to/folder")),
+                Some(&String::from("file.txt"))
+            ),
+            Some(String::from("path/to/folder/file.txt"))
+        );
+
+        // Test case 5: Only filename_format is provided, with placeholder
+        assert_eq!(
+            get_full_format_path(None, Some(&String::from("%original_filename"))),
+            Some(String::from("%original_filename"))
+        );
+
+        // Test case 6: Both folder_format and filename_format are provided, with placeholder
+        assert_eq!(
+            get_full_format_path(
+                Some(&String::from("path/to/folder")),
+                Some(&String::from("%original_filename"))
+            ),
+            Some(String::from("path/to/folder/%original_filename"))
+        );
+    }
+
+    #[test]
+    fn test_get_organization_strategy_copy() {
+        let strategy = OrganizationMode::Copy;
+        let result = get_organization_strategy(Some(&strategy));
+        assert!(result.is_some());
+    }
+
+    #[test]
+    fn test_get_organization_strategy_symlinks() {
+        let strategy = OrganizationMode::Symlinks;
+        let result = get_organization_strategy(Some(&strategy));
+        assert!(result.is_some());
+    }
+
+    #[test]
+    fn test_get_organization_strategy_move() {
+        let strategy = OrganizationMode::Move;
+        let result = get_organization_strategy(Some(&strategy));
+        assert!(result.is_some());
+    }
+
+    #[test]
+    fn test_get_organization_strategy_none() {
+        let result = get_organization_strategy(None);
+        assert!(result.is_none());
+    }
 }
